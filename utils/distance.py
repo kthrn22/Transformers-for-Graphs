58d3c01141adf4d3017732c1af57bff6bdf1df5b
Nhan Tieu <mynhantieu1309@gmailcom>
2023-01-09 18:51:04 -0800

    Edit distance.py

--- utils/distance.py
+++ utils/distance.py
@@ -1,24 +1,24 @@
 import torch
 import numpy as np
 
-def distances(coordinates):
+def distance(node_pos):
     r"""
     Parameters:
-        coordinate (torch.tensor): 
+        node_pos (torch.Tensor): 
             3D Coordinates of each node. Shape: [N, 3]
 
     Returns:
-        distances (torch.tensor):
+        distance (torch.Tensor):
             3D distance between each pair of nodes. Shape: [N, N]
     """    
-    num_nodes = coordinates.shape[0]
+    num_nodes = node_pos.shape[0]
     # shape [N, N, 3]
-    duplicated_coordinates = torch.broadcast_to(coordinates.unsqueeze(0), (num_nodes, num_nodes, 3))
+    duplicated_coordinates = torch.broadcast_to(node_pos.unsqueeze(0), (num_nodes, num_nodes, 3))
     # permute to [N, 3, N]
     duplicated_coordinates = duplicated_coordinates.permute(0, 2, 1)
 
     # shape [N, 3, N]
-    distances = torch.broadcast_to(coordinates.unsqueeze(-1), duplicated_coordinates.shape) - duplicated_coordinates
+    distances = torch.broadcast_to(node_pos.unsqueeze(-1), duplicated_coordinates.shape) - duplicated_coordinates
     # shape [N, 3, N] -> shape [N, N]
     distances = torch.sum(distances ** 2, dim = 1) ** 0.5
 
